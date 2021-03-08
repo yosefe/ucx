d52808e35de33a7d220d5e32f27cdce131336902
Yossi Itigin <yosefe@mellanox.com>
2021-03-08 21:28:12 +0200

    UCS/SOCK: Fix RPM build with gcc11 on fedora34

--- src/ucs/sys/sock.c
+++ src/ucs/sys/sock.c
@@ -293,6 +293,7 @@ ucs_status_t ucs_socket_server_init(const struct sockaddr *saddr, socklen_t sock
     uint16_t port;
 
     /* Create the server socket for accepting incoming connections */
+    fd     = -1; /* Suppress compiler warning */
     status = ucs_socket_create(saddr->sa_family, SOCK_STREAM, &fd);
     if (status != UCS_OK) {
         goto err;
