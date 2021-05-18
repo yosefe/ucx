780254c41fd4285aee0741490c0a74adaab742bf
dmitrygx <dmitrygla@mellanox.com>
2021-05-18 19:09:40 +0300

    APPS/IODEMO: Fix unknown conn_id happened when only one peer is fully connected

--- test/apps/iodemo/ucx_wrapper.cc
+++ test/apps/iodemo/ucx_wrapper.cc
@@ -454,6 +454,7 @@ void UcxContext::add_connection(UcxConnection *conn)
 {
     assert(_conns.find(conn->id()) == _conns.end());
     _conns[conn->id()] = conn;
+    UCX_LOG << "added " << conn->get_log_prefix() << " to connection map";
 }
 
 void UcxContext::remove_connection(UcxConnection *conn)
@@ -461,6 +462,8 @@ void UcxContext::remove_connection(UcxConnection *conn)
     conn_map_t::iterator i = _conns.find(conn->id());
     if (i != _conns.end()) {
         _conns.erase(i);
+        UCX_LOG << "removed " << conn->get_log_prefix()
+                << " from connection map";
     }
 }
 
@@ -873,6 +876,7 @@ void UcxConnection::connect_common(ucp_ep_params_t &ep_params,
                  << _conn_id;
 
     connect_tag(callback);
+    _context.add_connection(this);
 }
 
 void UcxConnection::established(ucs_status_t status)
