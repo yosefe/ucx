1204878c67b722903f8661abab3c7f5af9c15d65
Yossi Itigin <yosefe@nvidia.com>
2021-09-15 19:05:21 +0300

    WIP tools/info: print distance of 2 devices

--- src/tools/info/ucx_info.c
+++ src/tools/info/ucx_info.c
@@ -12,6 +12,7 @@
 
 #include <ucs/config/parser.h>
 #include <ucs/config/global_opts.h>
+#include <ucs/sys/string.h>
 #include <ucm/api/ucm.h>
 #include <getopt.h>
 #include <stdlib.h>
@@ -62,6 +63,7 @@ static void usage() {
            "                  endpoint in client/server mode\n");
     printf("  -6              IPv6 address specified with option -A\n");
     printf("  -T              Print system topology\n");
+    printf("  -x <d1>,<d2>    Print system distance between d1 and d2\n");
     printf("  -M              Print memory copy bandwidth\n");
     printf("  -h              Show this help message\n");
     printf("\n");
@@ -86,8 +88,10 @@ int main(int argc, char **argv)
     uint64_t ucp_features;
     size_t ucp_num_eps;
     size_t ucp_num_ppn;
-    unsigned print_opts;
+    int print_opts;
     char *tl_name, *mem_size;
+    const char *dev1 = NULL, *dev2 = NULL;
+    char devbuf[128];
     const char *f;
     int c;
 
@@ -103,7 +107,7 @@ int main(int argc, char **argv)
     ucp_ep_params.field_mask = 0;
     ip_addr_family           = AF_INET;
 
-    while ((c = getopt(argc, argv, "fahvc6ydbswpeCt:n:u:D:P:m:N:A:TM")) != -1) {
+    while ((c = getopt(argc, argv, "fahvc6ydbswpeCt:n:u:D:P:m:N:A:Tx:M")) != -1) {
         switch (c) {
         case 'f':
             print_flags |= UCS_CONFIG_PRINT_CONFIG | UCS_CONFIG_PRINT_HEADER | UCS_CONFIG_PRINT_DOC;
@@ -224,6 +228,17 @@ int main(int argc, char **argv)
         case 'T':
             print_opts |= PRINT_SYS_TOPO;
             break;
+        case 'x':
+            ucs_strncpy_safe(devbuf, optarg, sizeof(devbuf));
+            dev1 = strtok(devbuf, ",");
+            dev2 = strtok(NULL, ",");
+            if ((dev1 == NULL) || (dev2 == NULL)) {
+                usage();
+                return -1;
+            }
+
+            print_opts |= PRINT_DEV_DISTANCE;
+            break;
         case 'M':
             print_opts |= PRINT_MEMCPY_BW;
             break;
@@ -253,7 +268,7 @@ int main(int argc, char **argv)
         print_type_info(tl_name);
     }
 
-    if ((print_opts & (PRINT_DEVICES | PRINT_SYS_TOPO)) ||
+    if ((print_opts & (PRINT_DEVICES | PRINT_SYS_TOPO | PRINT_DEV_DISTANCE)) ||
         (print_flags & UCS_CONFIG_PRINT_CONFIG)) {
         /* if UCS_CONFIG_PRINT_CONFIG is ON, trigger loading UCT modules by
          * calling print_uct_info()->uct_component_query()
@@ -261,8 +276,9 @@ int main(int argc, char **argv)
         print_uct_info(print_opts, print_flags, tl_name);
     }
 
-    if (print_opts & (PRINT_SYS_INFO | PRINT_MEMCPY_BW | PRINT_SYS_TOPO)) {
-        print_sys_info(print_opts);
+    if (print_opts & (PRINT_SYS_INFO | PRINT_MEMCPY_BW | PRINT_DEV_DISTANCE |
+                      PRINT_SYS_TOPO)) {
+        print_sys_info(print_opts, dev1, dev2);
     }
 
     if (print_flags & UCS_CONFIG_PRINT_CONFIG) {
