a1928c343eabf29877ec675e2abfcd69778ee129
Yossi Itigin <yosefe@nvidia.com>
2022-01-12 14:33:45 +0200

    UCT/CUDA: Fixes in whole-alloc registration
    - Improve documentation
    - Move functions that need to access cuda_copy_md from cuda/base to
      cuda/cuda_copy. In cuda/base, we cannot assume the md instance is
      uct_cuda_copy_md_t.

--- src/uct/cuda/base/cuda_md.h
+++ src/uct/cuda/base/cuda_md.h
@@ -7,13 +7,11 @@
 #define UCT_CUDA_MD_H
 
 #include <uct/base/uct_md.h>
+#include <cuda.h>
 
-ucs_status_t uct_cuda_base_detect_memory_type(uct_md_h md, const void *address,
-                                              size_t length,
-                                              ucs_memory_type_t *mem_type_p);
 
-ucs_status_t uct_cuda_base_mem_query(uct_md_h md, const void *address,
-                                     size_t length, uct_md_mem_attr_t *mem_attr);
+size_t uct_cuda_base_get_total_device_mem(CUdevice cuda_device);
+
 
 ucs_status_t
 uct_cuda_base_query_md_resources(uct_component_t *component,
